c3de67d53c420857bdeb0ad0e3a0d2cfd7a67887
Yoshua Meatchem <meatchemyoshua@yahoo.com>
2026-01-03 18:52:49 -0500

    initial commit for auto repair landing page

---
+++ src/assets/js/faq.js
@@ -0,0 +1,8 @@
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+for (const item of faqItems) {
+    const onClick = () => {
+        item.classList.toggle('active')
+    }
+    item.addEventListener('click', onClick)
+}
+                                
